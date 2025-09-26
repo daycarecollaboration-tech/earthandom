58a28308b2936c6519625c61112a7a720a8bd973
daycarecollaboration-tech <daycarecollaboration@gmail.com>
2025-09-26 12:50:29 -0400

    Update site changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,12 @@
-import * as React from "react";
+// src/pages/contact.js
+import React from "react";
 import Layout from "../components/Layout/Layout";
 import Contact from "../components/Contact/Contact";
 
-const ContactPage = () => {
-  return (
-    <Layout>
-      <Contact />
-    </Layout>
-  );
-};
+const ContactPage = () => (
+  <Layout>
+    <Contact />
+  </Layout>
+);
 
 export default ContactPage;
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,31 +1,13 @@
-import * as React from "react";
+import React from "react";
 import Layout from "../components/Layout/Layout";
-import "../styles/classes.css"; // optional reuse of styling
 
-const ThanksPage = () => {
-  return (
-    <Layout>
-      <section className="classes-hero">
-        <h1>Thank You!</h1>
-        <p>
-          Your message has been sent successfully. 🙏  
-          We’ll get back to you as soon as possible.
-        </p>
-      </section>
-
-      <section className="classes-info">
-        <h2>What’s Next?</h2>
-        <ul>
-          <li>You’ll receive a confirmation email shortly.</li>
-          <li>We usually respond within 24–48 hours.</li>
-          <li>
-            In the meantime, feel free to explore our{" "}
-            <a href="/classes">Classes & Memberships</a>.
-          </li>
-        </ul>
-      </section>
-    </Layout>
-  );
-};
+const ThanksPage = () => (
+  <Layout>
+    <section style={{ textAlign: "center", padding: "100px 20px" }}>
+      <h1>Thank You! 🙏</h1>
+      <p>Your message has been sent successfully. We’ll get back to you soon.</p>
+    </section>
+  </Layout>
+);
 
 export default ThanksPage;
